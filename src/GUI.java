1347eefcee10eb312a5938e4a46daa22de1a2751
student <evelyns60@nycstudents.net>
2023-05-23 10:48:30 -0400

    Initial commit

---
+++ src/GUI.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class GUI extends JFrame implements ActionListener, ItemListener{
+
+    public GUI() {
+        super("Frame title");
+        init();
+    }
+
+    private void init() {
+        //frame setup
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,500);
+        setLocation(300,50);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+
+    public void itemStateChanged(ItemEvent e) {
+        
+    }
+}
