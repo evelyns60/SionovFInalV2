260a8ed9077776a1bcd5928fe52e570be4cb59a1
student <evelyns60@nycstudents.net>
2023-05-26 10:51:12 -0400

    5/26/23 class push

--- src/GUI.java
+++ src/GUI.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GUI extends JFrame implements ActionListener, ItemListener {
-    private JPanel mainPanel = new JPanel();
+public class GUI extends JFrame implements ActionListener, ItemListener, KeyListener {
+    private JPanel mainPanel;
 
     public GUI() {
         createUIComponents();
@@ -11,18 +11,37 @@ public class GUI extends JFrame implements ActionListener, ItemListener {
 
     private void createUIComponents() {
         //frame setup
-        setContentPane(mainPanel);
+        mainPanel = new JPanel();
+        add(mainPanel);
         setTitle("My GUI!");
         setSize(1500,950);
         setLocation(300,50);
+        getContentPane().setBackground(Color.cyan);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        setLayout(new GridLayout(6, 7, 3, 3));
+        //creates title panel
+        JPanel titlePanel = new JPanel();
+        JLabel gameTitle = new JLabel("Welcome to Connect 4!");
+        titlePanel.add(gameTitle);
+
+        //creates connect4 grid
+        mainPanel.setLayout(new GridLayout(3, 1));
+        mainPanel.add(titlePanel);
+
+        JPanel gamePanel = new JPanel();
+        gamePanel.setLayout(new GridLayout(6, 7, 10, 10));
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
-                add(new Button());
+                JButton newButton = new JButton();
+                newButton.setBackground(Color.blue);
+                gamePanel.add(newButton);
             }
         }
+
+        mainPanel.add(gamePanel);
+
+
+
         setVisible(true);
     }
 
@@ -31,6 +50,18 @@ public class GUI extends JFrame implements ActionListener, ItemListener {
     }
 
     public void itemStateChanged(ItemEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
 
     }
 }
